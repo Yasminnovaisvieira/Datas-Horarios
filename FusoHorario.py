53e3013200543d5c9a8e5881decc3022b364af60
Yasminnovaisvieira <yasminnovaisvieira789@gmail.com>
2025-04-20 14:40:44 -0300

    Datas e Horários

---
+++ FusoHorario.py
@@ -0,0 +1,15 @@
+import pytz #Trabalha com os fusos horários do mundo todo.
+import datetime
+
+print("Olá! O programa irá solicitar o nome de um país e informar o horário atual dele.\n")
+
+print("FORMATO A SER PERQUISADO: Região/Cidade (1ª letra maiúscula, sem espaços e em inglês).\n")
+
+print("Acesse esse site para verificar as opções disponíveis: https://mljar.com/blog/list-pytz-timezones/\n")
+pais = input("Digite o país que deseja saber o horário: ")
+
+if pais in pytz.all_timezones:
+    horario = datetime.datetime.now(pytz.timezone(pais))
+    print(horario.strftime("%d/%m/%Y %H:%M:%S"))
+else:
+    print("País não encontrado. Tente novamente.")
